96b173e5a97f50da1a14ef0ad296ca3a95572201
dvdluiz <dvdluiz1991@gmail.com>
2022-01-19 19:26:34 -0300

    start typeScript

---
+++ dist/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//string, boolean, number ...
+let x = 10;
+x = 20;
+console.log(x);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//string, boolean, number ...
+
+let x:number = 10;
+
+x = 20;
+
+console.log(x);
